48b67413e87ca94fa0182c3a5d4c5eff09483727
jumail12 <jumailjumi2003@gmail.com>
2024-10-18 10:17:05 +0530

    Time: 1 ms (100%), Space: 50.3 MB (54.46%) - LeetHub

--- 3567-convert-date-to-binary/3567-convert-date-to-binary.js
+++ 3567-convert-date-to-binary/3567-convert-date-to-binary.js
@@ -3,12 +3,9 @@
  * @return {string}
  */
 var convertDateToBinary = function(date) {
-   
    let[year,month,day]=date.split("-");
+return  `${parseInt(year).toString(2)}-${parseInt(month).toString(2)}-${parseInt(day).toString(2)}`;
 
- let a= `${parseInt(year).toString(2)}-${parseInt(month).toString(2)}-${parseInt(day).toString(2)}`;
-
- return a;
 
  
 
