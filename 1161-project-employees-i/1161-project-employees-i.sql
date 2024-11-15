1775e10584a0f66d7ab3314c6d4ab5a30bd004c6
jumail12 <jumailjumi2003@gmail.com>
2024-11-15 09:35:37 +0530

    Time: 550 ms (65.23%), Space: 0B (100%) - LeetHub

---
+++ 1161-project-employees-i/1161-project-employees-i.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select Project.project_id ,round(avg(experience_years),2) as average_years
+from Project
+join Employee on Employee.employee_id = Project.employee_id
+group by Project.project_id 
