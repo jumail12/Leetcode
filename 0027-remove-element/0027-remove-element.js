22b6db10a9e603f504a0992df933609fd5b77224
jumail12 <jumailjumi2003@gmail.com>
2024-12-12 10:05:32 +0530

    Time: 0 ms (100%), Space: 48.7 MB (91.03%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -4,20 +4,13 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-   let k = 0; // Counter for elements that are not equal to val
-
-    // Iterate through the array
-    for (let i = 0; i < nums.length; i++) {
-        // If the current element is not equal to val
-        if (nums[i] !== val) {
-            // Assign it to the k-th position in the array
-            nums[k] = nums[i];
-            // Increment the counter
+    let k=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]!==val){
+            nums[k]=nums[i];
             k++;
         }
     }
 
-    // The first k elements in nums are now the elements that are not equal to val
     return k;
-    
 };
