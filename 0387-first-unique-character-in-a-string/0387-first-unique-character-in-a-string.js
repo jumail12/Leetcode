4145e80de73be7f2f1c197c1da09e896a13098a8
jumail12 <jumailjumi2003@gmail.com>
2024-12-13 09:41:04 +0530

    Time: 31 ms (76.37%), Space: 52.4 MB (91.91%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    for(let i=0;i<s.length;i++){
+        if(s.indexOf(s.charAt(i))==s.lastIndexOf(s.charAt(i))){
+            return i;
+        }
+
+    }
+
+    return -1;
+};
