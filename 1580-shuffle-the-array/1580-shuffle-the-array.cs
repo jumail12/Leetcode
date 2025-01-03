8fedcac46fc4703e229f5ea132a4c7a03de2e14c
jumail12 <jumailjumi2003@gmail.com>
2025-01-03 09:32:20 +0530

    Time: 127 ms (46.61%), Space: 48.7 MB (39.84%) - LeetHub

---
+++ 1580-shuffle-the-array/1580-shuffle-the-array.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int[] Shuffle(int[] nums, int n) {
+        int[] res= new int[2*n];
+
+        for(int i=0; i<n;i++){
+            res[2*i]=nums[i];
+            res[(2*i)+1]=nums[i+n];
+        }
+
+        return res;
+        
+    }
+}
